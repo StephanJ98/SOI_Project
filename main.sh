fea58bac72ab1e78044ee94101e934d2a128fa0b
root <diego.jeandon@gmail.com>
2020-11-25 17:41:34 +0100

    First Commit

---
+++ main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Crear un script con comandos de Linux que admita dos o más argumentos;
+# de forma que si no es así debe visualizar el mensaje Error de sintaxis. 
+# Una vez comprobado esto, debe presentar el siguiente menú por pantalla y
+# programar, mediante funciones, las opciones según se detallan a continuación. 
+# Una vez terminada cada opción debe volver a mostrarse el menú y 
+# así sucesivamente hasta que escoja la opción “FIN”
