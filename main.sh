7468e4cad80e1700b38b3ff5ffa9266308d83b04
root <diego.jeandon@gmail.com>
2020-12-02 18:48:05 +0100

    Proyecto completo.

--- main.sh
+++ main.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Crear un script con comandos de Linux que admita dos o más argumentos;
-# de forma que si no es así debe visualizar el mensaje Error de sintaxis. 
-# Una vez comprobado esto, debe presentar el siguiente menú por pantalla y
-# programar, mediante funciones, las opciones según se detallan a continuación. 
-# Una vez terminada cada opción debe volver a mostrarse el menú y 
-# así sucesivamente hasta que escoja la opción “FIN”
 
 # Función que muestra el menu principal.
 function showMenu {
@@ -25,8 +19,30 @@ function fin {
 	exit
 }
 
-# Función DOBLE
+# Función CONTAR
+function contar {
+    read -p 'Introduce el nombre de un nuevo fichero: ' nombre
+    test -f $nombre && echo "Ese fichero ya existe." || wc -cwl $* > $nombre
+}
 
+# Función RESTO
+function resto {
+	local arr=($*)
+	printf "El resto es: "
+	for((i=0;i<${#arr[*]}-1;i++))
+		do
+			if(( ${arr[i]} == 0 ))
+			then
+				echo "No puedes dividir entre 0"
+			else 
+				let resto=${arr[i]}%${arr[i+1]}
+				printf "$resto "
+			fi
+	done
+	echo
+}
+
+# Función DOBLE
 function doble {
 	local arr=($*)
 	if (( ${arr[0]} <= ${arr[1]} ))
@@ -62,13 +78,13 @@ then
 			fin
 		elif [ $optVar == 'c' ] || [ $optVar == 'C' ]
 		then
-			echo "Función Contar"
+			contar $*
 		elif [ $optVar == 'd' ] || [ $optVar == 'D' ]
 		then
 			doble $*
 		elif [ $optVar == 'r' ] || [ $optVar == 'R' ]
 		then
-			echo "Función Resto"
+			resto $*
 		else 
 			echo "Opción no valida"
 		fi
